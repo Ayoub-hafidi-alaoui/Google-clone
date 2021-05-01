7f2e39b27f98ce2ba01d3c961528ec4f7cbb22a7
hafidi-alaoui <ayoub.hafidi-alaoui@ecole.ensicaen.fr>
2021-05-01 03:27:26 +0200

    add some buttons adn other features

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,14 +2,22 @@ import React from 'react'
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import "./Search.css"
+import { Button } from '@material-ui/core';
 
 function Search() {
+    const search=(e)=>{
+        e.preventDefault();
+    }
     return (
         <div className="search">
             <div className="search__input">
-                <SearchIcon className="serch__inputIcon"/>
+                <SearchIcon className="search__inputIcon"/>
                 <input/>
                 <MicIcon/>
+            </div>
+            <div className="search__buttons">
+                <Button onClick={search} </Button> variant="outlined" >Google search</Button>
+                <Button variant="outlined">I'm feeling lucky</Button>
             </div>
         </div>
     )
